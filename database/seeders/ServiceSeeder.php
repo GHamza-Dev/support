d16bbe8d32f9d41a277513ebaf76205ad31f83ac
ghamza-dev <elgassaihamzam54@gmail.com>
2022-04-17 03:23:41 +0200

    Create service factory & seeder

---
+++ database/seeders/ServiceSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Service;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Service::factory(6)->create();
+    }
+}
