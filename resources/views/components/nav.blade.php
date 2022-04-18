8ef856c3f5fe38beab2769ba6e7914b476796bed
ghamza-dev <elgassaihamzam54@gmail.com>
2022-04-18 09:20:30 +0200

    Add rout links

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -4,8 +4,8 @@
     </div>
     <nav>
         <ul class="flex flex-1 justify-between items-center">
-            <li class="mx-2 text-slate-600 hover:text-purple-900 hover:underline"><a href="#">Add ticket</a></li>
-            <li class="mx-2 text-slate-600 hover:text-purple-900 hover:underline"><a href="#">Tickets</a></li>
+            <li class="mx-2 text-slate-600 hover:text-purple-900 hover:underline"><a href="{{ route('create.ticket') }}">Add ticket</a></li>
+            <li class="mx-2 text-slate-600 hover:text-purple-900 hover:underline"><a href="{{ route('ticket.all') }}">Tickets</a></li>
             @if (!Auth::check())
                 <li class="mx-2 border bg-blue-600 text-slate-50 p-2 rounded-md hover:scale-105"><a href="{{ route('login') }}">Login</a></li>
                 <li class="mx-0 border bg-blue-600 text-slate-50 p-2 rounded-md hover:scale-105"><a href="{{ route('register') }}">Register</a></li>     
