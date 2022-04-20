45a3b1df939d9a2628d5f04005184237d84429a8
ghamza-dev <elgassaihamzam54@gmail.com>
2022-04-20 03:27:41 +0200

    Remove unnecessary file and fix broken routes

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 // --> User: ticket
 
 Route::prefix('ticket')->group(function(){
