36e9152db036586594b91e48d5f17df7825eabe5
ghamza-dev <elgassaihamzam54@gmail.com>
2022-04-23 02:06:22 +0200

    Add hover effect

--- resources/views/admin/sub/aside.blade.php
+++ resources/views/admin/sub/aside.blade.php
@@ -4,28 +4,28 @@
             Support
         </a>
         <ul class="mt-7">
-            <li class="relative px-6 py-3">
+            <li class="relative px-6 py-3 hover:bg-purple-600 hover:text-white transition-all duration-500 rounded-l-full">
                 <i class="fa-solid fa-house-chimney"></i>
                 <a href="{{ route('admin.tickets') }}">
                     <span>Dashboard</span>
                 </a>
             </li>
-            <li class="relative px-6 py-3">
+            <li class="relative px-6 py-3 hover:bg-purple-600 hover:text-white transition-all duration-500 rounded-l-full">
                 <a href="{{ route('services.all') }}">
                     <span class="ml-4">Services</span>
                 </a>
             </li>
-            <li class="relative px-6 py-3">
+            <li class="relative px-6 py-3 hover:bg-purple-600 hover:text-white transition-all duration-500 rounded-l-full">
                 <a href="#">
                     <span class="ml-4">Status</span>
                 </a>
             </li>
-            <li class="relative px-6 py-3">
+            <li class="relative px-6 py-3 hover:bg-purple-600 hover:text-white transition-all duration-500 rounded-l-full">
                 <a href="{{ route('users.all') }}">
                     <span class="ml-4">Users</span>
                 </a>
             </li>
-            <li class="relative px-6 py-3">
+            <li class="relative px-6 py-3 hover:bg-purple-600 hover:text-white transition-all duration-500 rounded-l-full">
                 <a href="{{ route('ticket.all') }}">
                     <span class="ml-4">User view</span>
                 </a>
