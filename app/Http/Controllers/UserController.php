e3b5ae7fa6645876074174c9b9914cdd9f557693
ghamza-dev <elgassaihamzam54@gmail.com>
2022-04-24 15:38:58 +0200

    Get services with users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Service;
 
 class UserController extends Controller
 {
     
     public function index()
     {
-        return view('admin.users',['users'=>User::getUsers()]);
+        return view('admin.users',['users'=>User::getUsers(),'services'=>Service::all()]);
     }
 
     public function destroy($id)
